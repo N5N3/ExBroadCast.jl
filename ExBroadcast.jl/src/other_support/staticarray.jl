b5312d529d0cc282ed7486da999e986cdf8ac613
N5N3 <2642243996@qq.com>
2021-05-30 15:32:33 +0800

    clean up

--- ExBroadcast.jl/src/other_support/staticarray.jl
+++
@@ -1,4 +0,0 @@
-import .StaticArrays: StaticArrayStyle, StaticArray
-mtb_copy(bc::Broadcasted{<:StaticArrayStyle}) = copy(bc) # I believe no one need it
-mtb_copyto!(dest::StaticArray, bc::Broadcasted{Nothing}) = copyto!(dest, bc) # I believe no one need it
-_similar(bc::Broadcasted{<:StaticArrayStyle}, T) = throw("StaticArrays is not supported for non-inplace @tab")
