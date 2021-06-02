7073ce6965559b713e1ea1042f825de9e1dcb234
N5N3 <2642243996@qq.com>
2021-06-02 16:15:50 +0800

    some fix

--- src/macro_kernal/tab.jl
+++ src/macro_kernal/tab.jl
@@ -1,9 +1,9 @@
 ## broadcast
-tab_broadcast(f::Tf, As...) where {Tf} = tab_materialize(broadcasted(f, As...))
+tab_broadcast(f::F, As...) where F = broadcasted(f, As...) |> tab_materialize
 
 ## materialize
 @inline tab_materialize(x) = x
-@inline tab_materialize(bc::Broadcasted) = tab_copy(instantiate(bc))
+@inline tab_materialize(bc::Broadcasted) = instantiate(bc) |> tab_copy
 
 @inline function materialize!(
     dest::NTuple{N,AbstractArray},
@@ -15,21 +15,16 @@ end
 
 ## copyto
 @inline tab_copy(bc::FilledBC) = copy(bc)
+@inline tab_copy(bc::Broadcasted) = copyto!(toa_similar(bc), bc) |> parent
 @inline function tab_copy(bc::Broadcasted{Style{Tuple}})
     dim = axes(bc)
     length(dim) == 1 || throw(DimensionMismatch("tuple only supports one dimension"))
     ElType = typeof(bc[1])
-    ElType <: Tuple{Any,Vararg{Any}} && Base.isconcretetype(ElType) || 
+    ElType <: Tuple{Any,Vararg{Any}} && Base.isconcretetype(ElType) ||
         throw("$ElType is not a legal return type for @tab!")
     @inline maketuple(x, y) = tuple(x, y...)
     @inline getind(k) = @inbounds _broadcast_getindex(bc, k)
     N = length(dim[1])
     N <= 16 && return mapfoldr(getind, (x, y) -> maketuple.(x, y), ntuple(identity,Val(N)))
     mapfoldr(getind, (x, y) -> maketuple.(x, y), dim[1])
-end
-
-@inline function tab_copy(bc::Broadcasted)
-    dest = toa_similar(bc)
-    copyto!(dest, bc)
-    parent(dest)
 end
